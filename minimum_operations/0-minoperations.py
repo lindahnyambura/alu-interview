fb0474efc37339391ac0107cf103ab350a180756
Lindah <l.nyambura@alustudent.com>
2024-01-21 21:51:19 +0300

    .

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''Minimum Operations'''
+
+def min_operations(n: int, result: int = 0) -> int:
+    '''
+    Returns the minimum number of operations needed to result in exactly n 'H' characters in the file.
+    '''
+    if n <= 1:
+        return 0
+    elif n <= 3:
+        
+        return n
+
+    common_factor = get_hcf(n)
+    return min_operations(common_factor) + (n // common_factor)
+
+
+def get_hcf(num: int) -> int:
+    '''
+    Returns the highest common factor (HCF) of a given number.
+    '''
+    for i in range(num // 2, 0, -1):
+        if num % i == 0:
+            return i
+
+
+if __name__ == "__main__":
+    pass
+
