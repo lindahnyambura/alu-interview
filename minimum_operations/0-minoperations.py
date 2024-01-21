8b30b228660c26714db09e16235c04f6613a76bc
Lindah <l.nyambura@alustudent.com>
2024-01-21 22:31:17 +0300

    ...

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 '''Minimum Operations'''
 
-def min_operations(n: int, result: int = 0) -> int:
+
+def min_operations(n, result=0):
     '''
     Returns the minimum number of operations needed to result in exactly n 'H' characters in the file.
     '''
     if n <= 1:
-        return 0
+        return (0)
     elif n <= 3:
-        return n
+        return (n)
 
     common_factor = get_hcf(n)
     return min_operations(common_factor) + (n // common_factor)
+
 
 def get_hcf(num: int) -> int:
     '''
     Returns the highest common factor (HCF) of a given number.
     '''
-    if num % 2 == 0:
-        return num // 2
     for i in range(num // 2, 0, -1):
         if num % i == 0:
-            return i
+            return (i)
